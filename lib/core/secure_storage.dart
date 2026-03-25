6ec9e30eb4032b7f13ea7d34b6aeb7d8aa6972ce
Conspiciens <fernandezjohann08@gmail.com>
2026-03-24 17:28:54 -0700

    Removed unused code & improved security

--- lib/core/secure_storage.dart
+++ lib/core/secure_storage.dart
@@ -1,11 +1,14 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:bcrypt/bcrypt.dart';
 import 'package:uuid/uuid.dart';
 
 /* Singleton to Manage the Secure Storage from IOS and Android */
 class SecureStorage {
   static SecureStorage? _instance; 
-  final FlutterSecureStorage _storage = FlutterSecureStorage(); 
+  final FlutterSecureStorage _storage = FlutterSecureStorage(
+    iOptions: IOSOptions(
+      accessibility: KeychainAccessibility.passcode
+    )
+  ); 
 
   factory SecureStorage() {
     _instance ??= SecureStorage._internal(); 
@@ -23,60 +26,22 @@ class SecureStorage {
     if (key == "") return; 
 
     if (isWallet) {
-      await _storage.write(key: key, value: pass); 
+      await _storage.write(key: key, value: pass, iOptions: IOSOptions(
+        accessibility: KeychainAccessibility.passcode
+      )); 
       return; 
     } 
 
     var UUID = Uuid(); 
     var uid = UUID.v4();
 
-    await _storage.write(key: key, value: uid);
+    await _storage.write(key: key, value: uid, iOptions: IOSOptions(
+      accessibility: KeychainAccessibility.passcode
+    ));
   }
 
   Future<void> removeKey(String key) async {
     if (key == "") return; 
     await _storage.delete(key: key); 
-  }
-
-  Future<void> writeStorage(String key, String walletName, String value) async {
-    if (key == "") return; 
-    if (value == "") return; 
-
-    final String passHash = BCrypt.hashpw(value, BCrypt.gensalt()); 
-    final String key_t = "$key\\_t"; 
-
-    await _storage.write(key: key_t, value: walletName); 
-    await _storage.write(key: key, value: passHash); 
-  }
-
-  Future<String?> getWalletName(String key) async {
-    if (key == "") return null;
-    final String key_t = "$key\\_t"; 
-
-    return await _storage.read(key: key_t); 
-  }
-
-  Future<(String, String)> readFileAndPass(String key) async {
-    String? pass = await _storage.read(key: key); 
-    String? jsonString = await _storage.read(key: "$key\\_f"); 
-
-    if (pass == null || jsonString == null) { throw Exception("Error retrieving form Secure Storage"); }
-
-    return (pass, jsonString); 
-  }
-
-  Future<void> writeFileAndPassStorage(String key, String walletName, String jsonString, String pass) async {
-    if (jsonString.isEmpty) {
-      throw Exception("Empty Json file"); 
-    } else if (pass.isEmpty) {
-      throw Exception("Password is empty");
-    }
-
-    final String passHash = BCrypt.hashpw(pass, BCrypt.gensalt()); 
-    final String key_t = "$key\\_t"; 
-
-    await _storage.write(key: key, value: passHash); 
-    await _storage.write(key: key_t, value: walletName); 
-    await _storage.write(key: "$key\\_f", value: jsonString);
   }
 }
