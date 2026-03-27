bc9cf3f0dc01cc1fa81eb7095079cbd64bba4659
Conspiciens <fernandezjohann08@gmail.com>
2026-03-27 11:06:33 -0700

    Added classes type

---
+++ lib/classes/coin.dart
@@ -0,0 +1,8 @@
+
+class Coin {
+  final String code; 
+  final String price; 
+  final String dateTime; 
+
+  Coin({required this.code, required this.price, required this.dateTime});
+}
---
+++ lib/classes/trades.dart
@@ -0,0 +1,7 @@
+class Trade {
+    final double timestamp; 
+    final double lastTradePrice; 
+
+    Trade({required this.timestamp, required this.lastTradePrice}); 
+}
+
