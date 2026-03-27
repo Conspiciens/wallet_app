9d2fa965547ceea292dbd58b699c62b99707049c
Conspiciens <fernandezjohann08@gmail.com>
2026-03-27 00:13:43 -0700

    Added ability to add wallet if it already exists

--- lib/core/rpc_server.dart
+++ lib/core/rpc_server.dart
@@ -1,25 +1,16 @@
+import 'dart:core';
 import 'package:http/http.dart'; 
 import 'package:wallet/wallet.dart';
 import 'package:web3dart/web3dart.dart';
 
-class Wallet {
-    final API_KEY = String.fromEnvironment("API_KEY"); 
-    final API_URL = String.fromEnvironment("API_URL"); 
-
-    final Client httpClient; 
+class WalletInfo {
+    final Web3Client ethClient; 
     
-    Wallet({required this.httpClient}); 
+    WalletInfo({required this.ethClient}); 
     
-    Future<EtherAmount> getBalance(Credentials creds) async {
-      if (API_URL == "") throw Exception("No API URL found"); 
-      if (API_KEY == "") throw Exception("No API Key found"); 
-
-      final fullApiUrl = API_URL + API_KEY; 
-      final ethClient = Web3Client(fullApiUrl, httpClient); 
-
-      final address = creds.address; 
-    
-      return await ethClient.getBalance(address); 
+    Future<EtherAmount> getBalance(EthereumAddress addr) async {
+      return await ethClient.getBalance(addr); 
     }
 
+    Future<void> sendTransaction(Credentials creds) async {}
 }
